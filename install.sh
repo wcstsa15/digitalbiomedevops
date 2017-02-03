365d8fbc6c54e500216b5c153da06428320e646d
Vilhelm <wcstsa15@gmail.com>
2017-02-03 17:37:15 -0600

    Avoiding chef for now.

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-# This runs as root on the server
-# Are we on a vanilla system?
 export DEBIAN_FRONTEND=noninteractive
 # Upgrade headlessly (this is only safe-ish on vanilla systems)
 apt-get update &&
 apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade &&
     # Install Ruby and Chef
-apt-get install -y ruby build-essential ruby-dev
-gem install --no-rdoc --no-ri chef
+#apt-get install -y ruby build-essential ruby-dev
+#gem install --no-rdoc --no-ri chef
+apt-get install nginx
+
+ufw allow 'Nginx HTTP'
+ufw allow 'OpenSSH'
+ufw allow 'ssh'
+ufw --force enable
+
+reboot
