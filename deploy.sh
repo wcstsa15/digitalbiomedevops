fbecb6ec7d35c42318a5be0962c217d8516389e1
Vilhelm <wcstsa15@gmail.com>
2017-02-03 12:22:46 -0600

    Editor issues

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,9 @@ host="${1:-root@138.197.114.17}"
 # The host key might change when we instantiate a new VM, so
 # we remove (-R) the old host key from known_hosts
 #tar cpf . | ssh -tt -o 'StrictHostKeyChecking no' "$host" ' --Old
+# -tt causes error on tar trying to read from terminal
+# absence of -tt causes error by not passing password
+# need to update sudo nopasswd. but that kind of defeats the purpose of sudo.
 ssh-keygen -R "${host#*@}" 2> /dev/null
 
 tar cpf - . | ssh -o 'StrictHostKeyChecking no' "$host" '
